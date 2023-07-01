302d8b41434e6b319726e56f564a907940b67d12
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-02 03:41:11 +0530

    Search HeroIcon implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,43 @@
 import React from 'react'
 import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline"
 
 function Header() {
-    return (
-        <header>
-          {/* Top Nav */}
-          <div className='flex items-center bg-vmart_blue flex-grow py-2'>
-              <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-                  <Image 
-                  src='https://links.papareact.com/f90'
-                  width={150}
-                  height={40}
-                  objectFit='contain'
-                  className='cursor-pointer'
-                  />
-              </div>
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className='flex items-center bg-vmart_blue flex-grow py-2'>
+        <div className='mx-4 mt-2 flex items-center flex-grow sm:flex-grow-0'>
+          <Image
+            src='https://links.papareact.com/f90'
+            width={150}
+            height={40}
+            // objectFit='contain'
+            className='cursor-pointer'
+          />
+        </div>
 
-              {/* Search */}
-              <div>
-                <input type="text" />
-              </div>
-          </div>
+        {/* Search */}
+        <div className='m-4 hidden sm:flex items-center flex-grow h-10 rounded-md bg-yellow-400 hover:bg-yellow-500 cursor-pointer'>
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus: outline-none px-4"
+            type="text" />
+          <SearchIcon
+            className="h-12 p-4"
+          />
+        </div>
+      </div>
 
-          {/* Bottom Nav */}
-          <div>
+      {/* Bottom Nav */}
+      <div>
 
-          </div>
-        </header>
-    )
+      </div>
+    </header>
+  )
 }
 
 export default Header
