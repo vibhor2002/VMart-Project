e782156e68943ed53a78af00304f49febf5330ea
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-02 02:41:51 +0530

    Building the top Nav section of the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,20 @@ function Header() {
     return (
         <header>
           {/* Top Nav */}
-          <div>
-              <div>
+          <div className='flex items-center bg-vmart_blue flex-grow py-2'>
+              <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
                   <Image 
                   src='https://links.papareact.com/f90'
                   width={150}
                   height={40}
+                  objectFit='contain'
+                  className='cursor-pointer'
                   />
+              </div>
+
+              {/* Search */}
+              <div>
+                <input type="text" />
               </div>
           </div>
 
