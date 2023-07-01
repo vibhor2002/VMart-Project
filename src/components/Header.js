1a6fc773563e0405d4af6ebcbc19083880297376
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-02 02:31:49 +0530

    Creating the header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from "next/image";
+
+function Header() {
+    return (
+        <header>
+          {/* Top Nav */}
+          <div>
+              <div>
+                  <Image 
+                  src='https://links.papareact.com/f90'
+                  width={150}
+                  height={40}
+                  />
+              </div>
+          </div>
+
+          {/* Bottom Nav */}
+          <div>
+
+          </div>
+        </header>
+    )
+}
+
+export default Header
