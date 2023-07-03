f24fb7efc5b93327b05434530e3b6d9858b786fa
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-04 00:43:48 +0530

    Banner Componenet Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>VMart</title>
       </Head>
@@ -11,6 +12,13 @@ export default function Home() {
       
       {/* Header */}
       <Header />
+
+      <main className='max-w-screen-xl mx-auto'>
+        {/* Banner */}
+        <Banner />
+
+        {/* Product Feed */}
+      </main>
 
     </div>
   );
