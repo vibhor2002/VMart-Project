de9ed5a0082f097e35ef313936ff91fb9666174c
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-04 00:18:11 +0530

    Bottom Nav Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,8 +50,20 @@ function Header() {
       </div>
 
       {/* Bottom Nav */}
-      <div>
-
+      <div className='flex items-center space-x-3 p-2 pl-6 bg-vmart_blue-light text-white text-sm'>
+        <p className='flex items-center link'>
+          <MenuIcon className='h-6 mr-1'/>
+          All
+        </p>
+        <p className='link '>Prime Video</p>
+        <p className='link '>VMart Business</p>
+        <p className='link '>Today' Deals</p>
+        <p className='link hidden lg:inline-flex'>Electronics</p>
+        <p className='link hidden lg:inline-flex'>Food & Grocery</p>
+        <p className='link hidden lg:inline-flex'>Prime</p>
+        <p className='link hidden lg:inline-flex'>Buy Again</p>
+        <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
+        <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
       </div>
     </header>
   )
