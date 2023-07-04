b9a2ad71fddf5b7ba4e0814c754d135cdd21b93f
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-05 03:29:39 +0530

    Product Feed Component Done

--- src/components/Product.js
+++ src/components/Product.js
@@ -19,16 +19,16 @@ function Product({ id, title, price, description, category, image }) {
         <div className="relative flex flex-col m-5 bg-white z-30 p-10">
             <p className="absolute top-2 right-2 text-xs italic text-gray-500">{category}</p>
 
-            <div className="flex justify-center">
+            <div className="flex justify-center my-3">
                 <Image
                     src={image}
                     height={200}
                     width={200}
-                    style={{ objectFit: "contain" }}
+                    style={{ width: 200, height: 200,objectFit: "contain" }}
                     alt=""
                 />
             </div>
-            <h4>{title}</h4>
+            <h4 className="mt-4">{title}</h4>
             <div className="flex mt-2">
                 {Array(rating).fill().map((_, i) => (
                     <StarIcon key={i} className="h-5 text-yellow-500" />
@@ -47,7 +47,7 @@ function Product({ id, title, price, description, category, image }) {
             {hasPrime && (
                 <div className="flex items-center space-x-2 mt-2">
                     <img className="w-12 mb-2" src="https://static.vecteezy.com/system/resources/previews/005/261/540/non_2x/fast-delivery-icon-free-vector.jpg" alt="" />
-                    <p className="text-sm text-gray-500">FREE Next-Day Delivery</p>
+                    <p className="text-xs text-gray-500">FREE Next-Day Delivery</p>
                 </div>
             )}
 
