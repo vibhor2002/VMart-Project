63dbe6f6c6eedba453722ab47cadb67b14ef4dfc
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-06 03:06:48 +0530

    Installing NextAuth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+
+export default NextAuth({
+
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+
+    Providers.Google({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+  ],
+
+  database: process.env.DATABASE_URL,
+})
