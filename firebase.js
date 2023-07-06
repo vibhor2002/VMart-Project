e821638b12074bb9e5f4e961edc32babf39bf0c3
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-06 16:05:06 +0530

    Authorizing Redirect URL's

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxPSmkkfAmW5wqIPJ32I-yhzQF5-J3ZGY",
+    authDomain: "vmart-ffd3e.firebaseapp.com",
+    projectId: "vmart-ffd3e",
+    storageBucket: "vmart-ffd3e.appspot.com",
+    messagingSenderId: "583517014008",
+    appId: "1:583517014008:web:0bf314b096ffef6ab7f80d"
+  };
