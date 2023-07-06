37056df8ce19556e36798d0d14c6264584ca0e00
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-06 16:35:40 +0530

    Created the CheckOut component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,12 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline"
 import { signIn, signOut, useSession } from "next-auth/client"
+import { useRouter } from 'next/router';
 
 
 function Header() {
   const [session] = useSession();
+  const router = useRouter();
 
   return (
     <header>
@@ -18,6 +20,8 @@ function Header() {
       <div className='flex items-center bg-vmart_blue flex-grow py-2'>
         <div className='mx-4 mt-2 flex items-center flex-grow sm:flex-grow-0'>
           <Image
+
+            onClick={() => router.push('/')}
             src={vmart}
             width={150}
             height={40}
@@ -48,7 +52,7 @@ function Header() {
             <p className='font-extrabold md:text-sm'>& Orders</p>
           </div>
 
-          <div className='relative link flex items-center'>
+          <div onClick={() => router.push('/checkout')} className='relative link flex items-center'>
             <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-center text-black font-bold'>0</span>
             <ShoppingCartIcon className='h-10' />
             <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
---
+++ src/pages/checkout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function Checkout() {
+  return (
+    <div className='bg-gray-100'>
+        <Header/>
+
+        <main className='lg:flex max-w-screen-xl mx-auto'>
+          {/* Left */}
+
+          {/* Right */}
+        </main>
+    </div>
+  )
+}
+
+export default Checkout
