566f561ee6b54c07d1035fb39637e16e8b39c91a
Vibhor Grover <grovervibhor21@gmail.com>
2023-07-09 15:04:10 +0530

    Building Right side of Checkout Page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,9 +5,12 @@ import banner from '../../public/ban.png'
 import { useSelector } from 'react-redux'
 import { selectItems } from '../slices/basketSlice'
 import CheckoutProduct from '../components/CheckoutProduct'
+import Currency from 'react-currency-formatter';
+import { useSession } from 'next-auth/client'
 
 function Checkout() {
   const items = useSelector(selectItems);
+  const [session] = useSession();
 
   return (
     <div className='bg-gray-100'>
@@ -32,16 +35,16 @@ function Checkout() {
             </h1>
 
             {items.map((item, i) => (
-              <CheckoutProduct 
-              key={item.i}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              description={item.description}
-              category={item.category}
-              image={item.image}
-              hasPrime={item.hasPrime}
+              <CheckoutProduct
+                key={item.i}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
               />
             ))}
 
@@ -49,6 +52,22 @@ function Checkout() {
         </div>
 
         {/* Right */}
+        <div className='flex flex-col bg-white p-10 shadow-md'>
+          {items.length > 0 && (
+            <>
+              <h2 className='whitespace-nowrap'>
+                Subtotal ({items.length} items) :
+                <span className='font-bold'>
+                  {/* <Currency className="mb-5" quantity={total} currency="USD" /> */}
+                </span>
+              </h2>
+
+              <button disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-200'}`}>
+                {!session ? `Sign In to Checkout` : 'Proceed to Checkout'}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   )
